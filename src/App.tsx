c8210a09e69b35f3c15a58d2b2b991d1e1a9fb7d
Rami GB <ramigb@gmail.com>
2019-08-31 00:06:23 +0200

    Ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "spectre.css/dist/spectre.min.css";
+import "./App.css";
+import { About } from "./Components/Pages";
+import { Create } from "./Components/Playlist";
+import { Page } from "./Components/Pages";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <>
+          <header className="navbar">
+            <section className="navbar-section">
+              <span className="logo">PlayListGENER8R</span>
+            </section>
+            <section className="navbar-section">
+              <Link to="/create" className="btn btn-link">
+                Create
+              </Link>
+            </section>
+            <section className="navbar-center"></section>
+            <section className="navbar-section">
+              <Link to="/about">About</Link>
+              <a href="#" className="btn btn-link">
+                GitHub
+              </a>
+            </section>
+          </header>
+
+          <div className="container">
+            <Route exact path="/" component={Create} />
+            <Route exact path="/create" component={Create} />
+            <Route exact path="/p/:encodedPage" component={Page} />
+            <Route exact path="/about" component={About} />
+          </div>
+        </>
+      </Router>
     );
   }
 }
